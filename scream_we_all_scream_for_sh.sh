cde9b62f829879104ae413dedc602e2e22c48822
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-09-07 20:00:25 -0700

    a correction and a dream.

--- scream_we_all_scream_for_sh.sh
+++ scream_we_all_scream_for_sh.sh
@@ -2,7 +2,7 @@ function PASSALLERRORS(){
   # per request:
   say 'MICHAEL SCOTT "no GOD NOO"'
   ## future case, support different voices by changing mac defaults.
-  say $*
+  say $@
 }
 # built_in
 
@@ -10,3 +10,9 @@ function PASSALLERRORS(){
 # back in the day, wifi rabbits... little plastic things 
 # they had swivelly ears ... you could pass simple commands over http
 # if the test did not pass, say ${name} broke the build, broke the build.
+
+## THE DREAM
+## function stderr {
+##   builtin stderr $@ 
+##   doSomeThingElse
+## }
